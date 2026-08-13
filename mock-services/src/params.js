036f55aa7aa7204fb7c4529deb0ddc8adc6d6efb
zzannh <20231095@sungshin.ac.kr>
2026-08-13 22:25:49 +0900

    Delete mock-services directory

--- mock-services/src/params.js
+++
@@ -1,8 +0,0 @@
-function ctx(req) {
-  const uid = req.query.uid || req.body.uid || "demo";
-  const raw = req.query.attack ?? req.body.attack ?? "";
-  const attack = ["1", "true", "on"].includes(String(raw));
-  return { uid, attack };
-}
-
-module.exports = { ctx };
